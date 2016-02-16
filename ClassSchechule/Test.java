1812bd2dec304f110546482755d7f87efc03ffeb
pheei <phe@gwu.edu>
2016-02-15 23:52:56 -0500

    Update Test.java

--- ClassSchechule/Test.java
+++ ClassSchechule/Test.java
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
 package com.ood.project.ClassSchechule;
 
 import java.text.ParseException;
-=======
->>>>>>> origin/master
 
 public class Test {
 
@@ -15,46 +12,26 @@ public class Test {
 		String input3 = "Date, February 17, Building, Philip Hall, RoomNumber, B156, isEveryWeek, true";
 		String input4 = "Building, Philip Hall, RoomNumber, B156, isEveryWeek, true";
 
-<<<<<<< HEAD
 		
 		ClassInfoParser parser1 = new ClassInfoParser(new ForgivingClassBuilder());
-=======
-		//example for normally creating a new object
-		ClassInfoParser parser1 = new ClassInfoParser(new ClassRegisterBuilder());
->>>>>>> origin/master
 		parser1.parse(input1);
 		ClassRegister class1 = parser1.getNewObject();
 		System.out.println(class1);
 		
-<<<<<<< HEAD
 		
 		ClassInfoParser parser2 = new ClassInfoParser(new ForgivingClassBuilder());
-=======
-		//example for creating a new object with seperate inputs
-		ClassInfoParser parser2 = new ClassInfoParser(new ClassRegisterBuilder());
->>>>>>> origin/master
 		parser2.parse(input2a);
 		parser2.parse(input2b);
 		ClassRegister class2 = parser2.getNewObject();
 		System.out.println(class2);
 		
-<<<<<<< HEAD
 		ClassInfoParser parser3 = new ClassInfoParser(new ForgivingClassBuilder());
 		parser3.parse(input3);
-=======
-		//example for creating a new object with check and validation (insert default minimal number for studentNumber varable when missing such this parameter)
-		ClassInfoParser parser3 = new ClassInfoParser(new ClassRegisterBuilder());
->>>>>>> origin/master
 		parser3.parse(input3);
 		ClassRegister class3 = parser3.getNewObject();
 		System.out.println(class3);
 		
-<<<<<<< HEAD
 		ClassInfoParser parser4 = new ClassInfoParser(new ForgivingClassBuilder());
-=======
-		//example for throwing corresponding exceptions when required information missing
-		ClassInfoParser parser4 = new ClassInfoParser(new ClassRegisterBuilder());
->>>>>>> origin/master
 		parser4.parse(input4);
 		ClassRegister class4 = parser4.getNewObject();
 		System.out.println(class4);
