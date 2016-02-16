cbd0b75c563f85c6db343ba4e9ec4416bc259584
pheei <phe@gwu.edu>
2016-02-16 00:02:53 -0500

    Update ForgivingClassBuilder.java

--- ClassSchechule/ForgivingClassBuilder.java
+++ ClassSchechule/ForgivingClassBuilder.java
@@ -21,7 +21,11 @@ public class ForgivingClassBuilder extends ClassBuilder{
         boolean noStudentNumber = (studentNumber == 0);
 
         if (noStudentNumber) {
-        	studentNumber = 0;
+        	studentNumber = MINSTUDENTNUM;
+        }
+        
+        if (building==null){
+        	building = "TBD";
         }
 
         check();
@@ -36,8 +40,6 @@ public class ForgivingClassBuilder extends ClassBuilder{
         if (date == null)
             throw new BuilderException("Valid date not found");
 
-        if (building == null)
-            throw new BuilderException("Valid city not found");
 
         if (studentNumber < MINSTUDENTNUM)
             throw new BuilderException("Minimum headcount is " + MINSTUDENTNUM);
